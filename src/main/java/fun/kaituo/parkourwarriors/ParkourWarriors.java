334dbf7ed7de1ff18844d3d1e6a0257be6fa48fb
LeChocolatChaud <mazihan1024@hotmail.com>
2022-12-20 18:43:41 +0800

    bump to v3.0.0

--- src/main/java/fun/kaituo/parkourwarriors/ParkourWarriors.java
+++ src/main/java/fun/kaituo/parkourwarriors/ParkourWarriors.java
@@ -38,8 +38,9 @@ public final class ParkourWarriors extends JavaPlugin implements Listener {
     private Location startLocation;
     private Advancement[] pwAdvancements;
     private BoundingBox[] pwAdvancementsCriteriaBoxes;
-    private final BoundingBox endBoundingBox = new BoundingBox(-53.0, 57.0, -81.0, -49.6, 61.0, -78.6);
-    private final BoundingBox extraBoundingBox = new BoundingBox(30.0, -350.0, -30.0, 47.0, 81.0, 18.0);
+    private final BoundingBox endBoundingBox = new BoundingBox(-8.0, 88.0, -6.0, -9.0, 91.0, 11.0);
+    private final BoundingBox extraBoundingBox1 = new BoundingBox(30.0, -350.0, -30.0, 47.0, 81.0, 18.0);
+    private final BoundingBox extraBoundingBox2 = new BoundingBox(-41.0, -350.0, -42.0, -18.0, 81.0, -16.0);
 
     @Override
     public void onEnable() {
@@ -92,34 +93,38 @@ public final class ParkourWarriors extends JavaPlugin implements Listener {
                 new NamespacedKey(this, "lv2_part3"),               // 7
                 new NamespacedKey(this, "lv2_part4"),               // 8
                 new NamespacedKey(this, "lv2_part5"),               // 9
-                new NamespacedKey(this, "end"),                     // 10
-                new NamespacedKey(this, "going_back"),              // 11
-                new NamespacedKey(this, "lv1_sp1_honey"),           // 12
-                new NamespacedKey(this, "lv1_sp2_double_jump"),     // 13
-                new NamespacedKey(this, "lv2_sp1_quad_jump"),
-                new NamespacedKey(this, "lv2_sp2_honey_again")      // 15
+                new NamespacedKey(this, "lv3_start"),               // 10
+                new NamespacedKey(this, "end"),                     // 11
+                new NamespacedKey(this, "going_back"),              // 12
+                new NamespacedKey(this, "lv1_sp1_honey"),           // 13
+                new NamespacedKey(this, "lv1_sp2_double_jump"),     // 14
+                new NamespacedKey(this, "lv2_sp1_quad_jump"),       // 15
+                new NamespacedKey(this, "lv2_sp2_honey_again"),     // 16
+                new NamespacedKey(this, "lv3_sp_double_neo")        // 17
         };
-        pwAdvancements = new Advancement[15];
+        pwAdvancements = new Advancement[pwAdvancementKeys.length];
         for (int i = 0; i < pwAdvancementKeys.length; i++) {
             pwAdvancements[i] = Bukkit.getAdvancement(pwAdvancementKeys[i]);
         }
         pwAdvancementsCriteriaBoxes = new BoundingBox[]{
-                null,
-                new BoundingBox(5.0, 75.0, 62.5, 7.0, 77.0, 64.5),
-                new BoundingBox(10.0, 79.0, 67.0, 11.0, 81.0, 71.0),
-                new BoundingBox(38.0, 71.0, 7.0, 41.0, 77.0, 10.0),
-                new BoundingBox(39.0, 73.0, -48.0, 46.0, 78.0, -46.0),
-                new BoundingBox(16.0, 65.0, -75.0, 18.375, 68.0, -67.625),
-                new BoundingBox(-20.0, 58.25, -90.0, -18.0, 61.0, -86.0),
-                new BoundingBox(-20.0, 32.0, -76.0, -18.0, 35.0, -71.0),
-                new BoundingBox(-24.0, 57.0, -67.0, -200, 60.0, -65.0),
-                null,
-                null,
-                null,
-                new BoundingBox(5.5, 75.5, 56.0, 6.5, 78.0, 57.0),
-                new BoundingBox(33.0, 59.0, 45.0, 36.0, 57.0, 49.0),
-                new BoundingBox(-15.0, 55.0, -80.0, -14.0, 58.0, -79.0),
-                new BoundingBox(-29.0, 68.0, -83.0, -27.0, 68.0, -82.0)
+                null,                                                               // root
+                new BoundingBox(5.0, 75.0, 62.5, 7.0, 77.0, 64.5),                  // l1p1
+                new BoundingBox(10.0, 79.0, 67.0, 11.0, 81.0, 71.0),                // l1p2
+                new BoundingBox(38.0, 71.0, 7.0, 41.0, 77.0, 10.0),                 // l1p3
+                new BoundingBox(39.0, 73.0, -48.0, 46.0, 78.0, -46.0),              // l2s
+                new BoundingBox(16.0, 65.0, -75.0, 18.375, 68.0, -67.625),          // l2p1
+                new BoundingBox(-20.0, 58.25, -90.0, -18.0, 61.0, -86.0),           // l2p2
+                new BoundingBox(-20.0, 32.0, -76.0, -18.0, 35.0, -71.0),            // l2p3
+                new BoundingBox(-24.0, 57.0, -67.0, -200, 60.0, -65.0),             // l2p4
+                new BoundingBox(-45.0, 60.0, -48.0, -42.0, 62.0, -45.0),            // l2p5
+                new BoundingBox(-38.0, 70.0, -25.0, -35.0, 72.0, -22.0),            // l3s
+                null,                                                               // end
+                null,                                                               // going_back
+                new BoundingBox(8.0, 75.0, 57.0, 9.0, 77.5, 58.0),                  // l1sp1
+                new BoundingBox(33.0, 59.0, 45.0, 36.0, 57.0, 49.0),                // l1sp2
+                new BoundingBox(-15.0, 55.0, -80.0, -14.0, 58.0, -79.0),            // l2sp1
+                new BoundingBox(-29.0, 68.0, -83.0, -27.0, 71.0, -82.0),            // l2sp2
+                new BoundingBox(-50.0, 72.0, -31.0, -48.0, 74.0, -30.0)             // l3sp
         };
     }
     
@@ -194,13 +199,13 @@ public final class ParkourWarriors extends JavaPlugin implements Listener {
         checkAdvancement(p);
         if (endBoundingBox.contains(location.toVector())) {
             if (parkourPlayersTasksMap.containsKey(p.getUniqueId())) {
-                p.getAdvancementProgress(pwAdvancements[10]).awardCriteria("impossible");
+                p.getAdvancementProgress(pwAdvancements[11]).awardCriteria("impossible");
                 Bukkit.getScheduler().cancelTask(parkourPlayersTasksMap.get(p.getUniqueId()));
                 parkourPlayersTasksMap.remove(p.getUniqueId());
             }
             p.getInventory().remove(restartItemStack);
             p.getInventory().remove(stopItemStack);
-        } else if ((x * x + z * z > 1836.58 && x * x + z * z < 11946.49) || extraBoundingBox.contains(location.toVector())) {
+        } else if ((x * x + z * z > 1836.58 && x * x + z * z < 11946.49) || extraBoundingBox1.contains(location.toVector()) || extraBoundingBox2.contains(location.toVector())) {
             if (!parkourTeam.hasEntry(p.getName())) {
                 parkourTeam.addEntry(p.getName());
                 p.setScoreboard(parkourScoreboard);
@@ -226,7 +231,6 @@ public final class ParkourWarriors extends JavaPlugin implements Listener {
             BoundingBox box = pwAdvancementsCriteriaBoxes[i];
             AdvancementProgress ap = p.getAdvancementProgress(pwAdvancements[i]);
             if (box == null) continue;
-            if (ap.isDone()) continue;
             if (box.contains(location.toVector())) ap.awardCriteria("impossible");
         }
     }
