58a00b5995ad4060d99840f6923888df5700858f
LeChocolatChaud <mazihan1024@hotmail.com>
2022-12-20 19:07:55 +0800

    minor bug fixes

--- src/main/java/fun/kaituo/parkourwarriors/ParkourWarriors.java
+++ src/main/java/fun/kaituo/parkourwarriors/ParkourWarriors.java
@@ -41,7 +41,7 @@ public final class ParkourWarriors extends JavaPlugin implements Listener {
     private final BoundingBox endBoundingBox = new BoundingBox(-8.0, 88.0, -6.0, -9.0, 91.0, 11.0);
     private final BoundingBox extraBoundingBox1 = new BoundingBox(30.0, -350.0, -30.0, 47.0, 81.0, 18.0);
     private final BoundingBox extraBoundingBox2 = new BoundingBox(-41.0, -350.0, -42.0, -18.0, 81.0, -16.0);
-    private final BoundingBox extraBoundingBox3 = new BoundingBox(-52.0, 60.0, -11.0, -9.0, 10.0, 7.0);
+    private final BoundingBox extraBoundingBox3 = new BoundingBox(-52.0, 60.0, -11.0, -9.0, 110.0, 7.0);
 
     @Override
     public void onEnable() {
